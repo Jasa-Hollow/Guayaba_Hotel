3d6d8e80c57f1b89813c492531ebae8961c9152f
Jasa-Hollow <joel2262000@gmail.com>
2021-01-09 16:05:42 -0600

    Restart of Project

---
+++ .envExample.php
@@ -0,0 +1,11 @@
+<?php
+
+    Class Environment {
+        // Prueba...
+        public $user = "YOUR USER NAME";
+        public $pass = "PASSWORD";
+        public $host = "HOST NAME";
+        public $db = "DATABASE NAME";
+    }
+
+?>
